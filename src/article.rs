a1cbbb93e15303532e659a832e1a53a208b178d7
Mathieu Balland <mathieu.balland@dptechnology.com>
2020-04-21 16:24:43 -0700

    Implement an id for the article

--- src/article.rs
+++ src/article.rs
@@ -2,8 +2,12 @@ use chrono::{DateTime, Utc};
 use slugify::slugify;
 use std::path::PathBuf;
 
+#[derive(Debug, PartialEq)]
+pub struct ArticleId(String, String, Option<String>, String);
+
 /// Represent an article
 pub struct Article {
+    id: ArticleId,
     pub language: String,
     pub category: String,
     pub sub_category: Option<String>,
@@ -15,6 +19,49 @@ pub struct Article {
 }
 
 impl Article {
+    /// Returns a new empty article
+    pub fn from_language_category_title(
+        language: String, category: String, title: String) -> Self {
+        Self {
+            id: ArticleId(String::from(&language),
+                          String::from(&category),
+                          None,
+                          String::from(&title)),
+            language,
+            category,
+            sub_category: None,
+            title,
+            author: String::default(),
+            date: Utc::now(),
+            content: String::default(),
+            tags: vec![],
+        }
+    }
+
+    /// Returns a new empty article with sub-category
+    pub fn from_language_category_sub_category_title(
+        language: String, category: String, sub_category:String, title: String) -> Self {
+        Self {
+            id: ArticleId(String::from(&language),
+                          String::from(&category),
+                          Some(String::from(&sub_category)),
+                          String::from(&title)),
+            language,
+            category,
+            sub_category: Some(sub_category),
+            title,
+            author: String::default(),
+            date: Utc::now(),
+            content: String::default(),
+            tags: vec![],
+        }
+    }
+
+    /// Return the id
+    pub fn get_id(&self) -> &ArticleId {
+        &self.id
+    }
+
     /// Returns a slugify title
     pub fn get_slug(&self) -> String {
         slugify!(&self.title, separator = "_")
@@ -41,39 +88,54 @@ mod tests {
 
     #[test]
     fn test_article_get_path() {
-        let article = Article {
-            language: String::from("en"),
-            category: String::from("test"),
-            sub_category: None,
-            title: String::from("Title"),
-            author: String::from("me"),
-            date: Utc::now(),
-            content: String::from("content"),
-            tags: vec![],
-        };
+        let language = "en";
+        let category = "test";
+        let title = "title";
+
+        let article = Article::from_language_category_title(
+            String::from(language),
+            String::from(category),
+            String::from(title));
 
         let article_slug = article.get_slug();
-        let expected: PathBuf = ["en", "test", &article_slug].iter().collect();
 
-        assert_eq!(expected, article.get_path());
+        let expected_id = ArticleId(
+            String::from(language),
+            String::from(category),
+            None,
+            String::from(&article_slug));
+
+        let expected_path: PathBuf = ["en", "test", &article_slug].iter().collect();
+
+        assert_eq!(&expected_id, article.get_id());
+        assert_eq!(expected_path, article.get_path());
     }
 
     #[test]
     fn test_article_get_path_with_sub_category() { 
-        let article = Article {
-            language: String::from("en"),
-            category: String::from("test"),
-            sub_category: Some(String::from("sub")),
-            title: String::from("Title"),
-            author: String::from("me"),
-            date: Utc::now(),
-            content: String::from("content"),
-            tags: vec![],
-        };
+        let language = "en";
+        let category = "test";
+        let sub_category = "sub";
+        let title = "title";
+
+        let article = Article::from_language_category_sub_category_title(
+            String::from(language),
+            String::from(category),
+            String::from(sub_category),
+            String::from(title));
 
         let article_slug = article.get_slug();
-        let expected: PathBuf = ["en", "test", "sub", &article_slug].iter().collect();
 
-        assert_eq!(expected, article.get_path());
+        let expected_id = ArticleId(
+            String::from(language),
+            String::from(category),
+            Some(String::from(sub_category)),
+            String::from(&article_slug));
+
+        let expected_path: PathBuf =
+            ["en", "test", "sub", &article_slug].iter().collect();
+
+        assert_eq!(&expected_id, article.get_id());
+        assert_eq!(expected_path, article.get_path());
     }
 }
